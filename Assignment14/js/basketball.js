a45ce135625d779aa98f7c9c9b7c5d3fbe9c0056
CutterThatcher <cutter.thatcher@umontana.edu>
2023-12-14 16:33:07 -0700

    try

--- Assignment14/js/basketball.js
+++ Assignment14/js/basketball.js
@@ -14,6 +14,7 @@ ctx.moveTo(250, 500);
 ctx.lineTo(250, 100);
 ctx.moveTo(50, 300);
 ctx.lineTo(450, 300)
-ctx.moveTo(125, 150);
-ctx.quadraticCurveTo(125, 300, 125, 250);
+
+ctx.moveTo(108.6, 158.6);
+ctx.quadraticCurveTo(150, 300, 108.6, 441.4);
 ctx.stroke();
