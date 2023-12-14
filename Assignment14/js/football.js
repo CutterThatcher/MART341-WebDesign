b598dc0f8f3c9a4702affe254fd96b33ff547173
CutterThatcher <cutter.thatcher@umontana.edu>
2023-12-14 10:00:35 -0700

    curve

--- Assignment14/js/football.js
+++ Assignment14/js/football.js
@@ -1,11 +1,5 @@
-// football.js
-
-// Get the canvas element and its ctx
 var c = document.getElementById('FootballCanvas');
 var ctx = c.getContext('2d');
-
-// Clear the canvas
-ctx.clearRect(0, 0, c.width, c.height);
 
 // Set the colors and line width
 ctx.fillStyle = '#000'; // Black color for the football
@@ -17,13 +11,19 @@ ctx.beginPath();
 ctx.moveTo(250, 100); // Move to the starting point
 
 // Draw the top curve
-ctx.bezierCurveTo(100, 50, 400, 50, 250, 100);
-
-// Draw the left side curve
-ctx.bezierCurveTo(200, 150, 200, 250, 250, 300);
+ctx.quadraticCurveTo(250, 50, 450, 100);
 
 // Draw the bottom curve
-ctx.bezierCurveTo(300, 250, 300, 150, 250, 100);
+ctx.quadraticCurveTo(250, 150, 250, 100);
+
+// // Draw the top curve
+// ctx.bezierCurveTo(100, 50, 400, 50, 250, 100);
+
+// // Draw the left side curve
+// ctx.bezierCurveTo(200, 150, 200, 250, 250, 300);
+
+// // Draw the bottom curve
+// ctx.bezierCurveTo(300, 250, 300, 150, 250, 100);
 
 // Close the path and fill the football
 ctx.closePath();
