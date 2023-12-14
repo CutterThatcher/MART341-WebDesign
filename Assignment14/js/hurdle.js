7742b1d371f904d5ebbc9ff10e524f8afec7a50c
CutterThatcher <cutter.thatcher@umontana.edu>
2023-12-14 11:53:53 -0700

    hurdle

--- Assignment14/js/hurdle.js
+++ Assignment14/js/hurdle.js
@@ -3,39 +3,39 @@ var ctx = c.getContext("2d");
 
 // Draw the hurdle base
 ctx.beginPath();
-ctx.moveTo(100, 220); // Move to the starting point
-ctx.lineTo(300, 220); // Draw a line to the right
-ctx.lineTo(300, 200); // Draw a line up
-ctx.lineTo(100, 200); // Draw a line to the left
+ctx.moveTo(150, 365); // Move to the starting point
+ctx.lineTo(350, 365); // Draw a line to the right
+ctx.lineTo(350, 345); // Draw a line up
+ctx.lineTo(150, 345); // Draw a line to the left
 ctx.closePath(); // Close the path
 ctx.fillStyle = '#A9A9A9';
 ctx.fill(); // Fill the base
 
 // Draw the left post
 ctx.beginPath();
-ctx.moveTo(100, 200); //starting point
-ctx.lineTo(100, 100); //line up
-ctx.lineTo(110, 100); //line right
-ctx.lineTo(110, 200); //line down
+ctx.moveTo(150, 345); //starting point
+ctx.lineTo(150, 245); //line up
+ctx.lineTo(160, 245); //line right
+ctx.lineTo(160, 345); //line down
 ctx.closePath();
 ctx.fillStyle = '#9D123C';
 ctx.fill();
 
 // Draw the right post
 ctx.beginPath();
-ctx.moveTo(300, 200); //starting
-ctx.lineTo(300, 100); // line up
-ctx.lineTo(290, 100);// line left
-ctx.lineTo(290, 200);// line down
+ctx.moveTo(350, 345); //starting
+ctx.lineTo(350, 245); // line up
+ctx.lineTo(340, 245);// line left
+ctx.lineTo(340, 345);// line down
 ctx.closePath();
 ctx.fill();
 
 // Draw the crossbar
 ctx.beginPath();
-ctx.moveTo(95, 100); //starting
-ctx.lineTo(305, 100); //line right
-ctx.lineTo(305, 90); //line up
-ctx.lineTo(95, 90); //line left
+ctx.moveTo(145, 245); //starting
+ctx.lineTo(355, 245); //line right
+ctx.lineTo(355, 235); //line up
+ctx.lineTo(145, 235); //line left
 ctx.closePath();
 ctx.fillStyle = '#000000';
 ctx.fill();
